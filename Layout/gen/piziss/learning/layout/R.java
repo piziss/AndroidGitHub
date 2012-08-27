02d4bdb7c440f6f10d6dc3f9c0129370a2d47f46
piziss li <piziss.lw@gmail.com>
2012-08-27 22:10:51 +0800

    Relativelayout and nested layout
    TableLayout layout_span

--- Layout/gen/piziss/learning/layout/R.java
+++ Layout/gen/piziss/learning/layout/R.java
@@ -15,13 +15,25 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
+        public static final int cancelButton=0x7f07000b;
+        public static final int editTextRelative=0x7f070009;
+        public static final int firstTextViewFirstLinearNested=0x7f070002;
         public static final int firstTextViewLinear=0x7f070000;
-        public static final int menu_settings=0x7f070002;
+        public static final int firstTextViewSecondLinearNested=0x7f070005;
+        public static final int menu_settings=0x7f07000c;
+        public static final int okButton=0x7f07000a;
+        public static final int secondTextViewFirstLinearNested=0x7f070003;
         public static final int secondTextViewLinear=0x7f070001;
+        public static final int secondTextViewSecondLinearNested=0x7f070006;
+        public static final int textViewRelative=0x7f070008;
+        public static final int thirdTextViewFirstLinearNested=0x7f070004;
+        public static final int thirdTextViewSecondtLinearNested=0x7f070007;
     }
     public static final class layout {
         public static final int activity_liner_layout=0x7f030000;
-        public static final int activity_table_layout=0x7f030001;
+        public static final int activity_nested_layout=0x7f030001;
+        public static final int activity_relative_layout=0x7f030002;
+        public static final int activity_table_layout=0x7f030003;
     }
     public static final class menu {
         public static final int activity_liner_layout=0x7f060000;
@@ -31,6 +43,8 @@ public final class R {
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
         public static final int title_activity_liner_layout=0x7f040003;
+        public static final int title_activity_nested_layout=0x7f040005;
+        public static final int title_activity_relative_layout=0x7f040006;
         public static final int title_activity_table_layout=0x7f040004;
     }
     public static final class style {
---
+++ Layout/src/piziss/learning/layout/RelativeLayout.java
@@ -0,0 +1,14 @@
+package piziss.learning.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RelativeLayout extends Activity {
+	
+	@Override
+	public void onCreate(Bundle bundle) {
+		super.onCreate(bundle);
+		setContentView(R.layout.activity_relative_layout);
+	}
+
+}
